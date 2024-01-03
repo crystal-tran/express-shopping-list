913ce06a1da4c3e377c40f56bd2bad70b3470bad
Crystal Tran <c.t.tran95@gmail.com>
2024-01-03 18:38:37 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const { NotFoundError } = require("./expressError");
+
+app.use(express.json());                           // process JSON data
+app.use(express.urlencoded());                     // process trad form data
+
+// ... your routes go here ...
+
+
+/** 404 handler: matches unmatched routes. */
+app.use(function (req, res) {                      // handle site-wide 404s
+  throw new NotFoundError();
+});
+
+
+/** Error handler: logs stacktrace and returns JSON error message. */
+app.use(function (err, req, res, next) {           // global err handler
+  const status = err.status || 500;
+  const message = err.message;
+  if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
+  return res.status(status).json({ error: { message, status } });
+});
+
+module.exports = app;
